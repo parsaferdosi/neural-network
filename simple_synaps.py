2e097706afd20d198124902946485123d8c405aa
parsaferdosi <parsa78963210@gmail.com>
2025-03-06 14:26:50 +0330

    stillll first try with comments

--- simple_synaps.py
+++ simple_synaps.py
@@ -12,15 +12,15 @@ builder_inputs = NP.array([[0, 0, 1],
                            [0, 1, 1]]) # این داده‌های آموزشی است
 builder_outputs = NP.array([[0, 1, 1, 0]]).T # خروجی داده‌های آموزشی
 
-synaps_weight = NP.zeros((3, 1))
+synaps_weight = NP.ones((3, 1))#وزن سیناپس ها رو تعریف میکنیم
 print(synaps_weight)
 
 for i in range(100000): # بخش آموزش
-    input_layers = builder_inputs
-    output = sigmoid(NP.dot(input_layers, synaps_weight))
-    error = builder_outputs - output
-    adjustment = error * sigmoid_moshtagh(output)
-    synaps_weight += NP.dot(input_layers.T, adjustment)
+    input_layers = builder_inputs#ورودی آموزشی به یک متغیر داده میشود
+    output = sigmoid(NP.dot(input_layers, synaps_weight))#ضرب داخلی دو ماتریس وزن سیناپس و ورودی را به تابع سیگموید ارسال میکند
+    error = builder_outputs - output#ارور های نورون رو بررسی میکند
+    adjustment = error * sigmoid_moshtagh(output)#ارور هارو در مشتق تابع سیگموید که خروجی به عنوان ورودی بهش ارسال شده ضرب میکند
+    synaps_weight += NP.dot(input_layers.T, adjustment)#تصحیح خطاهای نورون
 
 print("weight of synaps after train:\n", synaps_weight)
 print("output:\n", output)
